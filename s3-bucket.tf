9f5eff921ec6d5bcbff4c34ebe6eb0177b83cf09
Razvan Ghita <rghita@example.com>
2022-06-24 13:52:34 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,11 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+module "s3-bucket" {
+  source  = "app.terraform.io/rghita-training/s3-bucket/aws"
+  version = "2.8.0"
 
-  bucket_prefix = "${var.prefix}"
+  bucket_prefix = "${var.prefix}-s3-bucket"
   acl    = "private"
 
   versioning = {
     enabled = true
   }
-
 }
